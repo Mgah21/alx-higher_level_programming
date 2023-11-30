b23930ad5a7829bf983c8b71a2fd0fe73c230297
MohammedGebril <mjbryl72@gmail.com>
2023-11-30 21:55:42 +0200

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-            print(result)
+import sys
+
+def main():
+    args = sys.argv[1:]
+    total = sum(int(arg) for arg in args)
+    print(total)
+
+    if __name__ == "__main__":
+        main()
