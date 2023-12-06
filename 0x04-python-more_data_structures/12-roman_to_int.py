58bd9c7c038e83881b8dd7933d7d86b43ebd43b0
MohammedGebril <mjbryl72@gmail.com>
2023-12-06 18:21:05 +0200

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    """
+    Converts a Roman numeral to an integer.
+
+    Args:
+    roman_string: A string representing a Roman numeral.
+
+    Returns:
+    An integer representing the value of the Roman numeral.
+    """
+
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_numeral_map = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+
     result = 0
-    prev_value = 0
-
-    for char in roman_string:
-        value = roman_numerals.get(char)
-        if value is None:
-            return 0
-
-        if prev_value < value:
-            result += value - 2 * prev_value
+    for i in range(len(roman_string)):
+        current_value = roman_numeral_map[roman_string[i]]
+        if i + 1 < len(roman_string) and roman_numeral_map[roman_string[i + 1]] > current_value:
+            result -= current_value
         else:
-            result += value
-
-            prev_value = value
+            result += current_value
 
             return result
+                                      
