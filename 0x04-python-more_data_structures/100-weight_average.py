1a8a09235eb6b7f84d60ef193176cbe649f5e37f
MohammedGebril <mjbryl72@gmail.com>
2023-12-06 21:05:30 +0200

    13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    """
-    Calculates the weighted average of a list of score-weight tuples.
-
-    Args:
-    my_list: A list of tuples containing integers (score, weight).
-
-    Returns:
-    The weighted average of the scores, or 0 if the list is empty.
-    """
-
+    """ returns the weighted average"""
+    """ of all integers tuple (<score>, <weight>)"""
     if not my_list:
         return 0
 
+    num = 0
+    den = 0
+
     for tup in my_list:
-        if not isinstance(tup[0], int) or not isinstance(tup[1], int):
-            return 0
+        num += tup[0] * tup[1]
+        den += tup[1]
 
-        num = 0
-        den = 0
-
-        for tup in my_list:
-            num += tup[0] * tup[1]
-            den += tup[1]
-
-            return (num / den)
-                                            
+    return (num / den)
